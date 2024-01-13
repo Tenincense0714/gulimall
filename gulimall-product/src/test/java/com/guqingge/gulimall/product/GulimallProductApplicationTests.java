240e13839401f5d0ca0278c164d0b6e7d619352b
guqingge <15814788760@163.com>
2024-01-13 12:50:51 +0800

    project commit

---
+++ gulimall-product/src/test/java/com/guqingge/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guqingge.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
